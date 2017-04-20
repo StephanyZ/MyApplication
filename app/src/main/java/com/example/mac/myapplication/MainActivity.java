ab59d7648052bc58d571cc07d1e1445a1d2d487e
StephanyZ <923036400@qq.com>
2017-04-20 13:03:29 +0800

    学习android布局以及调用service方法

--- app/src/main/java/com/example/mac/myapplication/MainActivity.java
+++ app/src/main/java/com/example/mac/myapplication/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.mac.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.content.Intent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    String msg = "Android : ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event");
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    // Method to start the service
+    public void startService(View view) {
+        startService(new Intent(getBaseContext(), MyService.class));
+    }
+
+    // Method to stop the service
+    public void stopService(View view) {
+        stopService(new Intent(getBaseContext(), MyService.class));
     }
 }
---
+++ app/src/main/java/com/example/mac/myapplication/MyService.java
@@ -0,0 +1,30 @@
+package com.example.mac.myapplication;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by mac on 17/4/20.
+ */
+
+public class MyService extends Service {
+    @Override
+    public IBinder onBind(Intent arg0) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // Let it continue running until it is stopped.
+        Toast.makeText(this, "服务已经启动", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "服务已经停止", Toast.LENGTH_LONG).show();
+    }
+}
