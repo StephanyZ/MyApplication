afee2399cacb66f8ec528e98130111cad05d47e4
StephanyZ <923036400@qq.com>
2017-04-22 15:05:17 +0800

    按钮添加渐变颜色，圆角等附加属性

--- app/src/main/java/com/example/mac/myapplication/Homepage.java
+++ app/src/main/java/com/example/mac/myapplication/Homepage.java
@@ -1,17 +1,33 @@
 package com.example.mac.myapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
-public class Homepage extends Activity {
+import static java.security.AccessController.getContext;
 
+public class Homepage extends Activity implements View.OnClickListener{
+    Button lookbtn,addbtn,outbtn,searchbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_homepage);
+        lookbtn=(Button)findViewById(R.id.lookbutton);
+        addbtn=(Button)findViewById(R.id.addbutton);
+        outbtn=(Button) findViewById(R.id.outbutton);
+        searchbtn=(Button)findViewById(R.id.searchbutton);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
